833ec3d8a52479f45ddb815c0378d0609347cb39
ArgoNeon <matveiganz@gmail.com>
2023-04-21 00:20:36 +0300

    time

--- time.cpp
+++ time.cpp
@@ -7,34 +7,37 @@
 int main(int argc, char **argv) {
         int size;
         int rank;
-        int transferNum = 1000000;
-        int num = 0;
+        int transferNum = 100000;
 	int i;
         int j;
+	int n = 6;
 
         MPI_Status status;
         MPI_Init(&argc, &argv);
         MPI_Comm_rank(MPI_COMM_WORLD, &rank);
         MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-  	if (rank == 0) {
-		clock_t start = clock();
-		for (i = 0; i < transferNum; i++) {
-			MPI_Send(&num, 1, MPI_INT, 1, 5, MPI_COMM_WORLD);
-			MPI_Recv(&num, 1, MPI_INT, 1, 5, MPI_COMM_WORLD, &status);
-		}
-		clock_t end = clock();
-		long double seconds = (long double)(end - start) / CLOCKS_PER_SEC;
-		printf("%Lf\n", seconds);
-		printf("%Lf микросекунд на коммуникацию между узлами\n", seconds * 1000000 / (2.0 * transferNum));
-	}	
+  	for (j = 0; j < n; j++) {
+		int power = pow(10, j);
+		int num[power];
+		if (rank == 0) {
+			clock_t start = 1000 * MPI_Wtime();
+			for (i = 0; i < transferNum; i++) {
+				MPI_Send(num, power, MPI_INT, 1, 5, MPI_COMM_WORLD);
+				MPI_Recv(num, power, MPI_INT, 1, 5, MPI_COMM_WORLD, &status);
+			}
+			clock_t end = 1000 * MPI_Wtime();
+			double seconds = (double)(end - start);
+			printf("%f микросекунд на коммуникацию между узлами буфера размером в %d элементов\n", seconds * 1000 / (2.0 * transferNum), power);
+		}	
 
-	if (rank == 1) {
-                for (i = 0; i < transferNum; i++) {
-                        MPI_Recv(&num, 1, MPI_INT, 0, 5, MPI_COMM_WORLD, &status);
-			MPI_Send(&num, 1, MPI_INT, 0, 5, MPI_COMM_WORLD);
-                }
-        }
+		if (rank == 1) {
+                	for (i = 0; i < transferNum; i++) {
+                        	MPI_Recv(num, power, MPI_INT, 0, 5, MPI_COMM_WORLD, &status);
+				MPI_Send(num, power, MPI_INT, 0, 5, MPI_COMM_WORLD);
+                	}
+        	}
+	}
 
         MPI_Finalize();
         return 0;
